68e5eff5d2b7f8b2db8431e45347940eea6ca2e5
vuongFE <vuong.do@alta.com.vn>
2022-05-16 14:22:14 +0700

    output managerment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from "lodash";
+import clickMe from "./print";
 console.log("run scripts");
 function component() {
   const element = document.createElement("div");
   element.innerHTML = _.join(["Hello", "webpack update v2 build", ""]);
+  const btn = document.createElement("button");
+  btn.innerHTML = "Click me and see console";
+  btn.onclick = clickMe;
+  element.appendChild(btn);
   return element;
 }
 document.body.appendChild(component());
